3d5165f235b00338c5433540ec7612ba93483818
dannielefendi <efendidanniel5@gmail.com>
2025-09-04 14:29:37 +0700

    Initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>© 2025 Cryptoscope. All Rights Reserved</p>
+    </div>
+  )
+}
+import './Footer.css'
+
+export default Footer
